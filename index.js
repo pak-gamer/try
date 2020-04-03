fab68e1031b48c88f19c9c9aa1fab9d0afe1c9db
taifshah <taifshah785@gmail.com>
2020-04-03 14:31:00 +0500

    Function added
    Adding some function to the bot.

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord=require('discord.js');
+const bot=new Discord.client()
+bot.on('ready',()=>{
+console.log("bot is online");
+bot.user.setActivity(`Say ??help`, { type: `WATCHING` });
+})
+
+bot.on('message', message=>{
+if(message.auther.bot){return;}
+if(message.content=="$upvote"){
+    message.reply("This feature is not yet avilable");
+}
+})
+
